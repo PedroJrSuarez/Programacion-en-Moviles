e4dd716859718c6347d7695b55d387d33a52341f
PedroJrSuarez <pedrojsuarez3005@gmail.com>
2026-08-26 21:25:54 -0500

    Proyecto inicial con menú del carrito

---
+++ Laboratorio 2/lab02-carrito-suarez/executeapp/src/main/java/com/suarez/executeapp/MyClass.kt
@@ -0,0 +1,7 @@
+package com.suarez.executeapp
+
+fun main() {
+    println("=========================================")
+    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+    println("=========================================")
+}
