2f5e96db86ed2ca15a2eb0ed5186c659c5b98802
PedroJrSuarez <pedrojsuarez3005@gmail.com>
2026-08-26 22:53:10 -0500

    Formato de reporte alineado con dos decimales

--- Laboratorio 2/lab02-carrito-suarez-con-IA/ExecuteApp/src/main/java/com/suarez/executeapp/MyClass.kt
+++ Laboratorio 2/lab02-carrito-suarez-con-IA/ExecuteApp/src/main/java/com/suarez/executeapp/MyClass.kt
@@ -18,6 +18,16 @@ fun calcularIGV(subtotal: Double): Double = subtotal * 0.18
 
 fun calcularTotal(subtotal: Double, igv: Double): Double = subtotal + igv
 
+fun mostrarDetalle(productos: List<Producto>) {
+    println("\nDETALLE DEL CARRITO")
+    var i = 1
+    for (p in productos) {
+        val importe = p.precio * p.cantidad
+        println(String.format("%d. %-20s x%d  S/ %8.2f", i, p.nombre, p.cantidad, importe))
+        i++
+    }
+}
+
 fun main() {
     println("=========================================")
     println("   CARRITO DE COMPRAS - TIENDA TECSUP    ")
@@ -40,11 +50,16 @@ fun main() {
         println("Producto agregado: ${prod.nombre}")
     }
 
+    mostrarDetalle(carrito)
+
     val subtotal = calcularSubtotal(carrito)
     val igv = calcularIGV(subtotal)
     val total = calcularTotal(subtotal, igv)
 
-    println("\nSubtotal    : S/ $subtotal")
-    println("IGV (18%)   : S/ $igv")
-    println("TOTAL       : S/ $total")
+    val totalItems = carrito.sumOf { it.cantidad }
+
+    println(String.format("Cantidad de productos : %d", totalItems))
+    println(String.format("Subtotal              : S/%8.2f", subtotal))
+    println(String.format("IGV (18%%)             : S/%8.2f", igv))
+    println(String.format("TOTAL A PAGAR         : S/%8.2f", total))
 }
